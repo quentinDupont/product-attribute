c54a908f8a0a14d6dc8832acbed52d1fd16e2949
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-11-14 10:35:14 +0100

    [MIG] product_sale_tax_price_included: Migration to 12.0

--- product_sale_tax_price_included/__manifest__.py
+++ product_sale_tax_price_included/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017, Grap
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Product sale tax price included",
     "summary": "See product sale price With and without Tax",
-    "version": "8.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Product",
     "website": "https://github.com/OCA/product-attribute",
     "author": "GRAP, Odoo Community Association (OCA)",
--- product_sale_tax_price_included/models/product_template.py
+++ product_sale_tax_price_included/models/product_template.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017, Grap
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
